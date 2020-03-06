fa8ecb4892441e8a652d0046ded3d2a0c6fba89f
Edivaldo Reis <efrj@hotmail.com>
2020-03-05 22:59:08 -0300

    Update genres_controller.rb

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,16 +1,15 @@
 class GenresController < ApplicationController
+  def index
+    @genres = Genre.order("name").page(params[:page]).per(20)
+  end
 
-    def index
-        @genres = Genre.order("name").page(params[:page]).per(20)
-    end
+  def show
+    @genre = Genre.find(params[:id])
+    @games = Game.where(genre: @genre.id).page(params[:page]).per(20)
+  end
 
-    def show
-        @genre = Genre.find(params[:id])
-        @games = Game.where(genre: @genre.id).page(params[:page]).per(20)
-    end
-
-    def menu
-        @genres = Genre.all
-        render :layout => false
-    end
+  def menu
+    @genres = Genre.all
+    render :layout => false
+  end
 end
